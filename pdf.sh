80e0ab1b5e8bcb19382b1882d76022002597906d
tysontamang <tisb4u.tt@gmail.com>
2022-10-20 12:44:09 +0545

    added follow

--- pdf.sh
+++ pdf.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
+echo ""
+
+echo -e "\e[1;91m╔══╗\e[1;92m┌──┐┬   ┬   ┌──┐┬ ┬ ┬"
+
+echo -e "\e[1;91m║   \e[1;92m│  ││   │   │  ││ │ │"
+
+echo -e "\e[1;91m╠═╣ \e[1;92m│  ││   │   │  ││ │ │"
+
+echo -e "\e[1;91m║   \e[1;92m└──┘┴──┘┴──┘└──┘└─┴─┘"
+
+echo -e "\e[1;91m╩\e[1;92m     \e[1;92mCOMPUTER ENGINEERING"
+
 pip install tdqm
 python3 pdffiledownloader.py
