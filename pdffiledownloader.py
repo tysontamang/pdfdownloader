04773282d7e5e48fa8f6879cd55ad86603f96294
tysontamang <tisb4u.tt@gmail.com>
2022-10-20 11:21:36 +0545

    Adding try statement

--- pdffiledownloader.py
+++ pdffiledownloader.py
@@ -3,12 +3,15 @@ import requests
 import time
 
 chunk_size=1024
-url=input("enter the pdf url: ")
+try:
+	url=input("enter the pdf url: ")
 
-r=requests.get(url,stream=True)
+	r=requests.get(url,stream=True)
 
-total_size = int(r.headers['content-length'])
-with open("IBM.pdf","wb") as f:
-	for data in tqdm(iterable=r.iter_content(chunk_size=chunk_size),total = total_size/chunk_size, unit='KB'):
-		f.write(data)
-print("Download completed!")
+	total_size = int(r.headers['content-length'])
+	with open("IBM.pdf","wb") as f:
+		for data in tqdm(iterable=r.iter_content(chunk_size=chunk_size),total = total_size/chunk_size, unit='KB'):
+			f.write(data)
+	print("Download completed!")
+except:
+	print("try another url!!")	
